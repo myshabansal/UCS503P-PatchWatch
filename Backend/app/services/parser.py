547f8cc177871f7b65b6291f64f59aa836080dc6
DURVA-GARGGG <dgarg9_be24@thapar.edu>
2026-08-31 12:06:58 +0530

    Add function to parse package.json dependencies

--- Backend/app/services/parser.py
+++ Backend/app/services/parser.py
@@ -1,1 +1,18 @@
+import json
 
+
+def parse_package_json(file_path: str):
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    dependencies = data.get("dependencies", {})
+
+    result = []
+
+    for name, version in dependencies.items():
+        result.append({
+            "name": name,
+            "version": version
+        })
+
+    return result
